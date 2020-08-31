8fcb8daf4ad210a7ec62f8e6dd61de84eabeda87
AntonVasko <vasko.anton.a@gmail.com>
2020-08-31 18:25:29 +0300

    Add files via upload

---
+++ 2. 31.08.2020/rect_red.py
@@ -0,0 +1,12 @@
+#основу коду запозичено з книги Джейсона Р. Бріггса "Python для дітей. Веселий вступ до програмування" с. 172
+from turtle import *
+
+t_pen = Turtle()
+t_pen.color ("red")
+t_pen.forward(50)
+t_pen.left(90)
+t_pen.forward(50)
+t_pen.left(90)
+t_pen.forward(50)
+t_pen.left(90)
+t_pen.forward(50)
