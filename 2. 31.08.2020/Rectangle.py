87bce47eab0c2b84337bb3c6fb6e56d293be438b
AntonVasko <vasko.anton.a@gmail.com>
2020-08-31 17:12:56 +0300

    Create Rectangle.py

---
+++ 2. 31.08.2020/Rectangle.py
@@ -0,0 +1,11 @@
+#основу коду запозичено з книги Джейсона Р. Бріггса "Python для дітей. Веселий вступ до програмування" с. 172
+import turtle
+
+t_pen = turtle.Pen()
+t_pen.forward(50)
+t_pen.left(90)
+t_pen.forward(50)
+t_pen.left(90)
+t_pen.forward(50)
+t_pen.left(90)
+t_pen.forward(50)
